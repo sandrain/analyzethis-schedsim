4b92324027ee078e2f5bed92b87dfca1fcafbeb1
Hyogi Sim <sandrain@gmail.com>
2014-04-21 09:56:40 -0400

    run_all.sh enhanced: check the file existence.

--- src/run_all.sh
+++ src/run_all.sh
@@ -4,6 +4,12 @@ pattern="workflows/$1*.xml"
 netbw="$((1*104857600))"
 runtime="1"
 
+ls $pattern 2>/dev/null
+if [ $? -ne 0 ]; then
+	echo "workflow not found"
+	exit 1
+fi
+
 for n in `seq 1 6`; do
     for workflow in $pattern; do
         nosd=$((2**n))
