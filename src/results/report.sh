9669d90744a17cf21be2d312b9b34fe2724d7d2f
Hyogi Sim <sandrain@gmail.com>
2014-04-25 14:06:37 -0400

    hostreduce/hostonly results added. report script changed to report all.

--- src/results/report.sh
+++ src/results/report.sh
@@ -9,46 +9,58 @@ w=$1
 
 ## report runtime
 echo "runtime"
-echo "nosd,rr,locality,minwait"
+echo "nosd,rr,locality,minwait,hostonly,hostreduce"
 for i in `seq 2 6`; do
 	frr="$((2**i))_rr_${w}_60.txt"
 	finput="$((2**i))_input_${w}_60.txt"
 	fminwait="$((2**i))_minwait_${w}_60.txt"
+	fhostonly="$((2**i))_hostonly_${w}_60.txt"
+	fhostreduce="$((2**i))_hostreduce_${w}_60.txt"
 	rr=`tail -n1 $frr | awk '{print $4}'`
 	input=`tail -n1 $finput | awk '{print $4}'`
 	minwait=`tail -n1 $fminwait | awk '{print $4}'`
-	echo $((2**i)),$rr,$input,$minwait
+	hostonly=`tail -n1 $fhostonly | awk '{print $4}'`
+	hostreduce=`tail -n1 $fhostreduce | awk '{print $4}'`
+	echo $((2**i)),$rr,$input,$minwait,$hostonly,$hostreduce
 done
 
 ## report utilization
 echo "osd utilization"
-echo "nosd,rr,locality,minwait"
+echo "nosd,rr,locality,minwait,hostonly,hostreduce"
 for i in `seq 2 6`; do
 	frr="$((2**i))_rr_${w}_60.txt"
 	finput="$((2**i))_input_${w}_60.txt"
 	fminwait="$((2**i))_minwait_${w}_60.txt"
+	fhostonly="$((2**i))_hostonly_${w}_60.txt"
+	fhostreduce="$((2**i))_hostreduce_${w}_60.txt"
 	rr=`egrep '^OSD mean utilization' $frr | awk '{print $5}'`
 	input=`egrep '^OSD mean utilization' $finput | awk '{print $5}'`
 	minwait=`egrep '^OSD mean utilization' $fminwait | awk '{print $5}'`
+	hostonly=`egrep '^OSD mean utilization' $fhostonly | awk '{print $5}'`
+	hostreduce=`egrep '^OSD mean utilization' $fhostreduce | awk '{print $5}'`
 	echo $((2**i)),$rr,$input,$minwait
 done
 
 ## report total data transfer
 echo "data transfer"
-echo "nosd,rr,locality,minwait"
+echo "nosd,rr,locality,minwait,hostonly,hostreduce"
 for i in `seq 2 6`; do
 	frr="$((2**i))_rr_${w}_60.txt"
 	finput="$((2**i))_input_${w}_60.txt"
 	fminwait="$((2**i))_minwait_${w}_60.txt"
+	fhostonly="$((2**i))_hostonly_${w}_60.txt"
+	fhostreduce="$((2**i))_hostreduce_${w}_60.txt"
 	rr=`egrep '^Total data' $frr | awk '{print $5}'`
 	input=`egrep '^Total data' $finput | awk '{print $5}'`
 	minwait=`egrep '^Total data' $fminwait | awk '{print $5}'`
+	hostonly=`egrep '^Total data' $fhostonly | awk '{print $5}'`
+	hostreduce=`egrep '^Total data' $fhostreduce | awk '{print $5}'`
 	echo $((2**i)),$rr,$input,$minwait
 done
 
 ## report write skewness
 echo "write skewness"
-echo "nosd,rr,locality,minwait"
+echo "nosd,rr,locality,minwait,hostonly,hostreduce"
 for i in `seq 2 6`; do
 	frr="$((2**i))_rr_${w}_60.txt"
 	rrmean=`egrep '^SSD mean write' $frr | awk '{print $5}'`
@@ -62,7 +74,15 @@ for i in `seq 2 6`; do
 	minwaitmean=`egrep '^SSD mean write' $fminwait | awk '{print $5}'`
 	minwaitstd=`egrep '^SSD std write' $fminwait | awk '{print $5}'`
 	minwait=`python -c "print float($minwaitstd)/float($minwaitmean)*100"`
-	echo $((2**i)),$rr,$input,$minwait
+	fhostonly="$((2**i))_hostonly_${w}_60.txt"
+	hostonlymean=`egrep '^SSD mean write' $fhostonly | awk '{print $5}'`
+	hostonlystd=`egrep '^SSD std write' $fhostonly | awk '{print $5}'`
+	hostonly=`python -c "print float($hostonlystd)/float($hostonlymean)*100"`
+	fhostreduce="$((2**i))_hostreduce_${w}_60.txt"
+	hostreducemean=`egrep '^SSD mean write' $fhostreduce | awk '{print $5}'`
+	hostreducestd=`egrep '^SSD std write' $fhostreduce | awk '{print $5}'`
+	hostreduce=`python -c "print float($hostreducestd)/float($hostreducemean)*100"`
+	echo $((2**i)),$rr,$input,$minwait,$hostonly,$hostreduce
 done
 
 
